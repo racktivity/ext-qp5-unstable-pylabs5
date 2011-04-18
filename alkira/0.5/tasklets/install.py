56132c9377bec9bf1d4e02ee8ecb55745f2884b9
deboeckj <devnull@localhost>
2011-04-18 13:07:00 +0000

    Remove configure tasklet of alkira

--- alkira/0.5/tasklets/install.py
+++ alkira/0.5/tasklets/install.py
@@ -5,4 +5,3 @@ def main(q, i, params, tags):
     qpackage = params['qpackage']
     for copyme in ("apps", "libexec", 'www'):
         q.system.fs.copyDirTree(q.system.fs.joinPaths(qpackage.getPathFiles(), "generic", copyme), q.system.fs.joinPaths(q.dirs.baseDir, copyme))
-    qpackage.signalConfigurationNeeded()
