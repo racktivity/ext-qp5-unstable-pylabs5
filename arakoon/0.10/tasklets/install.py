7149624865960d1d5f20dc40f35064b5953f74dc
deboeckj <devnull@localhost>
2011-04-19 07:09:15 +0000

    Fix bad install script of arakoon

--- arakoon/0.10/tasklets/install.py
+++ arakoon/0.10/tasklets/install.py
@@ -6,6 +6,3 @@ def main(q, i, params, tags):
     qpackage = params['qpackage']
     debfile = os.path.join(qpackage.getPathFiles(), 'linux64', 'arakoon_tip_amd64.deb' )
     q.platform.ubuntu.installDebFile(debfile)
-
-def match(q, i, param, tags):
-    return True
