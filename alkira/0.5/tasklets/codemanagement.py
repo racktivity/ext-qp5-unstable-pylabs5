3f4e432a1f265e39cb57d30b0f30f80e760a2e46
deboeckj <devnull@localhost>
2011-04-20 08:33:54 +0000

    Fixes

--- alkira/0.5/tasklets/codemanagement.py
+++ alkira/0.5/tasklets/codemanagement.py
@@ -17,7 +17,7 @@ def main(q, i, params, tags):
     q.system.fs.createDir(osisdir)
 
     recipe.addSource(connection, 'model/views/', q.system.fs.joinPaths(filesexportDir, 'views'))
-    recipe.addSource(connection, 'htdocs/', q.system.fs.joinPaths(filesexportDir, 'www'))
+    recipe.addSource(connection, 'htdocs/', q.system.fs.joinPaths(filesexportDir, 'www', 'lfw'))
     recipe.addSource(connection, 'services/lfw', q.system.fs.joinPaths(filesexportDir, 'lib', 'python', 'site-packages'))
     recipe.addSource(connection, 'scripts/', q.system.fs.joinPaths(filesexportDir, 'scripts'))
     recipe.addSource(connection, 'docs/', q.system.fs.joinPaths(filesexportDir, 'docs'))
--- pyapps_framework/0.5/tasklets/configure.py
+++ pyapps_framework/0.5/tasklets/configure.py
@@ -22,7 +22,7 @@ def main(q, i, params, tags):
     if PATH not in vhost.sites:
         site = vhost.addSite(PATH, '/%s' % PATH)
         site.addOption('root', ROOT)
-        site.addOption('rewrite ', '^/%s/(.*) /\$1 break' % PATH)
+        site.addOption('rewrite ', '^/%s/(.*) /$1 break' % PATH)
         site.addOption('rewrite  ', '^/%s$ /%s/ permanent' % (PATH, PATH))
     
     nginx.save()
