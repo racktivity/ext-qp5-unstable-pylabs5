702029985b598ebcfe3d0cda05052b64f26132b2
Jo De Boeck <jdeboeck@aserver.com>
2011-04-27 13:34:53 +0200

    Fix install issue in alkira

--- alkira/0.5/tasklets/install.py
+++ alkira/0.5/tasklets/install.py
@@ -4,4 +4,4 @@ __tags__   = 'install',
 def main(q, i, params, tags):
     qpackage = params['qpackage']
     for copyme in ("apps", "libexec", 'www', 'lib'):
-        q.system.fs.copyDirTree(q.system.fs.joinPaths(qpackage.getPathFiles(), "generic", copyme), q.system.fs.joinPaths(q.dirs.baseDir, copyme))
+        q.system.fs.copyDirTree(q.system.fs.joinPaths(qpackage.getPathFiles(), "generic", copyme), q.system.fs.joinPaths(q.dirs.baseDir, copyme), overwriteDestination=True)
