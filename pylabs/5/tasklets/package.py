03c45cfdc528153518515d145724df4cd174d4c8
Jo De Boeck <jdeboeck@aserver.com>
2011-05-17 09:56:03 +0200

    fix package tasklet

--- pylabs/5/tasklets/package.py
+++ pylabs/5/tasklets/package.py
@@ -4,7 +4,7 @@ __tags__ = 'package',
 
 def main(q, i, params, tags):
     qpackage = params["qpackage"]
-    qpfolder = q.system.fs.joinPaths(qpackage.getPathFiles(), 'generic'))
+    qpfolder = q.system.fs.joinPaths(qpackage.getPathFiles(), 'generic')
     q.system.fs.removeDirTree(qpfolder)
     filesexportDir = q.system.fs.joinPaths(q.dirs.varDir, 'src', qpackage.name, 'files')
 
