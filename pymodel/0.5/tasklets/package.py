2d3f79e0d27714c38c2a87d2b34aadc21d0a6e23
Ahmed Gamal <gamala@aserver.com>
2011-05-22 15:56:29 +0300

    Added recipe.json for pymodel

--- pymodel/0.5/tasklets/package.py
+++ pymodel/0.5/tasklets/package.py
@@ -2,13 +2,10 @@ __author__ = 'incubaid'
 __tags__ = 'package',
 
 def main(q, i, params, tags):
-    package = params["qpackage"]
-    
-    # Clean old files    
-    q.system.fs.removeDirTree(package.getPathFiles())
-    
-    # Copy source to files 
-    q.system.fs.copyDirTree(package.getPathSourceCode(), package.getPathFiles())
-    
+    qpackage = params["qpackage"]
+    filesDir = qpackage.getPathFiles()
+    q.system.fs.removeDirTree(filesDir)
+    q.system.fs.createDir(filesDir)
+    q.system.fs.copyDirTree(qpackage.getPathSourceCode(), qpackage.getPathFiles())
 
-    
+
