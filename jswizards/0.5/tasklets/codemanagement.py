9500130c53dfb9a20ddd05315c3ec7ace9fdb407
Mohammad Nour <nourm@incubaid.com>
2011-05-22 16:35:30 +0200

    Adding recipe.json to jswizards

--- jswizards/0.5/tasklets/codemanagement.py
+++ jswizards/0.5/tasklets/codemanagement.py
@@ -4,20 +4,5 @@ __tags__   = 'codemanagement',
 
 def main(q, i, params, tags):
     qpackage = params['qpackage']
-    repository = 'https://bitbucket.org/despiegk/jswizards/'
-    #updated to use the CoffeeScript branch instead of the default JS branch
-    branch = 'coffee'
-
-    from clients.mercurial.HgRecipe import HgRecipe
-    recipe = HgRecipe()
-    connection = i.config.clients.mercurial.findByUrl(repository)
-
-    filesexportDir = q.system.fs.joinPaths(q.dirs.varDir, 'src', qpackage.name, 'files')
-
-    if q.system.fs.exists(filesexportDir):
-        q.system.fs.removeDirTree(filesexportDir)
-
-    recipe.addRepository(connection)
-    recipe.addSource(connection, '', q.system.fs.joinPaths(filesexportDir, 'www', 'jswizards'), branch=branch)
-    recipe.executeTaskletAction(params['action'])
+    qpackage.checkoutFromRecipe()
 
--- jswizards/0.5/tasklets/package.py
+++ jswizards/0.5/tasklets/package.py
@@ -4,14 +4,12 @@ __tags__ = 'package',
 
 def main(q, i, params, tags):
     qpackage = params["qpackage"]
-    filesDir = q.system.fs.joinPaths(qpackage.getPathFiles())
+    filesDir = qpackage.getPathFiles()
     q.system.fs.removeDirTree(filesDir)
     q.system.fs.createDir(filesDir)
+    q.system.fs.copyDirTree(qpackage.getPathSourceCode(), qpackage.getPathFiles())
     filesexportDir = q.system.fs.joinPaths(q.dirs.varDir, 'src', qpackage.name, 'files')    
-   
     coffeePath = q.system.fs.joinPaths(filesexportDir, 'www', 'jswizards', 'js', 'jswizards.coffee')
     q.system.fs.removeFile(coffeePath)
-    
     q.system.fs.copyDirTree(filesexportDir, q.system.fs.joinPaths(filesDir, "generic"))
 
- 
