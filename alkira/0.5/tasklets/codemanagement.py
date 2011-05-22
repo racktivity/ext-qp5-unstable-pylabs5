068a36cdad6466e1465621e08b78c817830ca883
deboeckj <devnull@localhost>
2011-05-22 08:50:47 +0000

    Make recipe for alkira

--- alkira/0.5/tasklets/codemanagement.py
+++ alkira/0.5/tasklets/codemanagement.py
@@ -3,27 +3,4 @@ __tags__ = 'codemanagement',
 
 def main(q, i, params, tags):
     qpackage = params["qpackage"]
-    filesexportDir = q.system.fs.joinPaths(q.dirs.tmpDir, qpackage.name, qpackage.version )
-    q.system.fs.removeDirTree(filesexportDir)
-    osisdir = q.system.fs.joinPaths(filesexportDir, 'libexec', 'osis')
-
-    from clients.mercurial.HgRecipe import HgRecipe
-    recipe = HgRecipe()
-
-    connection =  i.config.clients.mercurial.findByUrl("http://bitbucket.org/despiegk/lfw")
-    recipe.addRepository(connection)
-    recipe.addSource(connection, 'model/rootobjects/', osisdir)
-
-    #recipe.addSource(connection, 'model/views/', q.system.fs.joinPaths(filesexportDir, 'views'))
-    recipe.addSource(connection, 'htdocs/', q.system.fs.joinPaths(filesexportDir, 'www', 'lfw'))
-    recipe.addSource(connection, 'services/lfw', q.system.fs.joinPaths(filesexportDir, 'lib', 'python', 'site-packages', 'alkira'))
-    recipe.addSource(connection, 'client', q.system.fs.joinPaths(filesexportDir, 'lib', 'pylabs', 'extensions', 'clients', 'alkira'))
-    #recipe.addSource(connection, 'docs/', q.system.fs.joinPaths(filesexportDir, 'docs'))
-    tmpdir = q.system.fs.joinPaths(filesexportDir, 'tmp')
-    recipe.addSource(connection, 'model/tasklets/', tmpdir)
-    osis_service_dir = q.system.fs.joinPaths(filesexportDir, 'apps', 'applicationserver', 'services', 'osis_service', 'tasklets')
-
-    recipe.executeTaskletAction(params["action"])
-
-    q.system.fs.copyDirTree(tmpdir, osis_service_dir)
-    q.system.fs.removeDirTree(tmpdir)
+    qpackage.checkoutRecipe()
--- alkira/0.5/tasklets/package.py
+++ alkira/0.5/tasklets/package.py
@@ -3,6 +3,8 @@ __tags__ = 'package',
 
 def main(q, i, params, tags):
     qpackage = params["qpackage"]
-    filesexportDir = q.system.fs.joinPaths(q.dirs.tmpDir, qpackage.name, qpackage.version )
-    q.system.fs.removeDirTree(qpackage.getPathFiles())
-    q.system.fs.copyDirTree(filesexportDir, q.system.fs.joinPaths(qpackage.getPathFiles(), "generic"))
+    filesDir = qpackage.getPathFiles()
+    q.system.fs.removeDirTree(filesDir)
+    q.system.fs.createDir(filesDir)
+    q.system.fs.copyDirTree(qpackage.getPathSourceCode(), qpackage.getPathFiles())
+
