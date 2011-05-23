01365d628fe06d6fe34e6ad197c9255817845ca3
Jo De Boeck <jdeboeck@aserver.com>
2011-05-23 11:01:36 +0200

    Fix compile tasklet for jswizards

--- jswizards/0.5/tasklets/compile.py
+++ jswizards/0.5/tasklets/compile.py
@@ -7,8 +7,7 @@ __tags__ = 'compile',
 
 def main(q, i, params, tags):
     qpackage = params["qpackage"]
-    filesexportDir = q.system.fs.joinPaths(q.dirs.varDir, 'src', qpackage.name, 'files')
-
+    filesexportDir = q.system.fs.joinPaths(qpackage.getPathSourceCode(), str(q.enumerators.PlatformType.GENERIC))
     compilePath = q.system.fs.joinPaths(filesexportDir, 'www', 'jswizards', 'js')
     q.system.process.run('coffee -c jswizards.coffee', cwd=compilePath)
 
--- jswizards/0.5/tasklets/package.py
+++ jswizards/0.5/tasklets/package.py
@@ -8,7 +8,7 @@ def main(q, i, params, tags):
     q.system.fs.removeDirTree(filesDir)
     q.system.fs.createDir(filesDir)
     q.system.fs.copyDirTree(qpackage.getPathSourceCode(), qpackage.getPathFiles())
-    filesexportDir = q.system.fs.joinPaths(q.dirs.varDir, 'src', qpackage.name, qpackage.version, str(q.enumerators.PlatformType.GENERIC))    
+    filesexportDir = q.system.fs.joinPaths(qpackage.getPathSourceCode(), str(q.enumerators.PlatformType.GENERIC))    
     coffeePath = q.system.fs.joinPaths(filesexportDir, 'www', 'jswizards', 'js', 'jswizards.coffee')
     q.system.fs.removeFile(coffeePath)
     q.system.fs.copyDirTree(filesexportDir, q.system.fs.joinPaths(filesDir, "generic"))
