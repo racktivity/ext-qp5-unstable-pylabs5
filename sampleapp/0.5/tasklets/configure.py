c5f5091d23e068981ce3aa3212e8c2550762ec42
Mohammad Nour <nourm@incubaid.com>
2011-05-29 14:30:37 +0200

    - Adding single qoutes around 'sampleapp' in autostart/stop commands.

--- sampleapp/0.5/tasklets/configure.py
+++ sampleapp/0.5/tasklets/configure.py
@@ -12,8 +12,8 @@ def main(q, i, p, params, tags):
     for appname in appnames:
         order = appnames.index(appname) + 1 * 10
         if not appname in i.config.autostart.list():
-            i.config.autostart.add(appname, {'command': 'p.application.start(%s)' %appname, 'order': str(order)})
+            i.config.autostart.add(appname, {'command': "p.application.start('%s')" %appname, 'order': str(order)})
 
         if not appname in i.config.autostop.list():
-            i.config.autostop.add(appname, {'command': 'p.application.stop(%s)' %appname, 'order': str(order)})
+            i.config.autostop.add(appname, {'command': "p.application.stop('%s')" %appname, 'order': str(order)})
 
