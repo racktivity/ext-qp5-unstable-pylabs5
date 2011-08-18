96700e018e326c5d1b5017fa38e80392fb3ca51d
kneirinc <devnull@localhost>
2011-08-18 10:52:27 +0200

    fix the old space names in the admin space for alkira 0.6

--- alkira/0.6/tasklets/install.py
+++ alkira/0.6/tasklets/install.py
@@ -13,12 +13,25 @@ def main(q, i, params, tags):
         for subdir in subdirs:
             ensureDecentStructure(subdir)
 
+    def changeSpacefileNames(pyappdir):
+        spacedirpath = q.system.fs.joinPaths(pyappdir, 'portal', 'spaces')
+        spacefilespath = q.system.fs.joinPaths(spacedirpath, 'Admin', 'Home', 'Spaces')
+        spacedirlist = q.system.fs.listDirsInDir(spacedirpath)
+        for fullDirname in spacedirlist:
+            dirname = q.system.fs.getBaseName(fullDirname)
+            if dirname == 'Admin' or dirname == 'IDE':
+                continue
+            filepath = q.system.fs.joinPaths(spacefilespath, dirname + '.md')
+            if q.system.fs.exists(filepath):
+                q.system.fs.renameFile(filepath, q.system.fs.joinPaths(spacefilespath,'s_' + dirname + '.md'))
 
     pyapps = q.system.fs.listDirsInDir(q.dirs.pyAppsDir)
     for pyapp in pyapps:
+        changeSpacefileNames(pyapp)
         spaces = q.system.fs.listDirsInDir(q.system.fs.joinPaths(pyapp, "portal", "spaces"))
         for space in spaces:
             ensureDecentStructure(space)
 
     qpackage = params['qpackage']
     qpackage.copyFiles()
+
