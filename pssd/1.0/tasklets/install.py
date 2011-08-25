a93a5fccae9bdf8193ecc4ba52536517d347ce14
Mohammad Nour <nourm@incubaid.com>
2011-08-25 17:41:26 +0200

    - Updated install tasklet.

--- pssd/1.0/tasklets/install.py
+++ pssd/1.0/tasklets/install.py
@@ -8,4 +8,5 @@ def main(q, i, params, tags):
     setup_path = q.system.fs.joinPaths(qpackage.getPathFiles(), str(q.enumerators.PlatformType.GENERIC), 'swift_security_delegate')
     q.system.process.run('python setup.py install', cwd=setup_path)
     q.system.unix.chown(q.system.fs.joinPaths(os.path.sep, 'usr', 'local', 'lib', 'python2.6', 'dist-packages', 'pssd-1.0-py2.6.egg'), 'root', 'root')
+    qpackage.copyFiles()
 
