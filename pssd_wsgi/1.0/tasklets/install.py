4a6083a5c458a54f0e0df452f81163cee15bd1ce
Mohammad Nour <nourm@incubaid.com>
2011-08-25 23:37:01 +0200

    - Updates.

--- pssd_wsgi/1.0/tasklets/install.py
+++ pssd_wsgi/1.0/tasklets/install.py
@@ -1,17 +1,16 @@
-
 __author__ = 'aserver'
 __tags__   = 'install',
+
+import os
 
 def main(q, i, params, tags):
     qpackage = params['qpackage']
     qpackage.copyFiles()
-    #q.platform.ubuntu.check()
-    #from pylabs.Shell import *
-    #ipshell()
-    #q.platform.ubuntu.updatePackageMetadata()
-    #q.platform.ubuntu.checkInstall("eric","eric")
-    #q.platform.ubuntu.install("python-qt4-sql")
-    #path=qpackage.getPathFiles()
-    #path=q.system.fs.joinPaths(path,"rootfs")
-    #q.system.fs.copyDirTree(path, "/")
+    setup_path = q.system.fs.joinPaths(q.dirs.tmpDir, 'swift_security_delegate')
+    egg_path = q.system.fs.joinPaths(os.path.sep, 'usr', 'local', 'lib', 'python2.6', 'dist-packages', 'pssd-%s-py2.6.egg' % qpackage.version)
+    q.system.fs.remove(egg_path, onlyIfExists=True)
+    q.system.process.run('python setup.py install', cwd=setup_path)
+    q.system.unix.chown(egg_path, 'root', 'root')
+    # We don't need setup path anymore
+    q.system.fs.removeDirTree(setup_path)
 
