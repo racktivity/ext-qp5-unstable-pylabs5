a6b767ee12f58434849caaaf7fc711a48cc65d8e
deboeckj <devnull@localhost>
2012-01-24 13:15:01 +0000

    New version of workflowengine

---
+++ workflowengine/5.0.1/tasklets/codemanagement.py
@@ -0,0 +1,10 @@
+__author__ = 'incubaid'
+__tags__     = "codemanagement",
+__priority__ = 1
+
+def match(q, i, params, tags):
+    return True
+
+def main(q, i, params, tags):
+    qpackage = params['qpackage']
+    qpackage.checkoutRecipe()
---
+++ workflowengine/5.0.1/tasklets/install.py
@@ -0,0 +1,13 @@
+__author__ = "incubaid"
+__tags__     = "install",
+__priority__ = 1
+
+def match(q, i, params, tags):
+    return True
+
+def main(q, i, params, tags):
+    # Copy the files for this platform from the files/ folder to sandbox
+    qpackage = params['qpackage']
+    py2_6 = q.system.fs.joinPaths(q.dirs.baseDir, 'lib', 'python2.6', 'site-packages', 'workflowengine')
+    if q.system.fs.exists(py2_6): q.system.fs.removeDirTree(py2_6)
+    qpackage.copyFiles()
